67d8d6ae9370553a8e11b241fb91a24d0f4f4e28
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-07-13 11:21:26 +0300

    2.3

--- library-backend/index.js
+++ library-backend/index.js
@@ -102,6 +102,7 @@ const typeDefs = `
     name: String!
     id: String!
     birthYear: Int
+    bookCount: Int!
   }
 
   type Book {
@@ -116,14 +117,19 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
+  Author: {
+    bookCount: root => books.filter(book => book.author === root.name).length
+  },
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
   },
 }
 
