23387a838746f255b2707f4adea62740e29121db
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-07-15 18:42:18 +0300

    Added missing labels

--- library-frontend/src/components/Login.jsx
+++ library-frontend/src/components/Login.jsx
@@ -39,17 +39,21 @@ const LoginForm = ({show, setToken}) => {
     <div>
       <form onSubmit={submit}>
         <div>
+          <label>
           username <input
             value={username}
             onChange={({ target }) => setUsername(target.value)}
           />
+          </label>
         </div>
         <div>
+          <label>
           password <input
             type='password'
             value={password}
             onChange={({ target }) => setPassword(target.value)}
           />
+          </label>
         </div>
         <button type='submit'>login</button>
       </form>
